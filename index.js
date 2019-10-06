5ed5df2177d7681dffbfe1c4bd06bb9215aaab2d
Njeri Ngigi <njery.ngigi@gmail.com>
2019-10-06 19:08:31 +0200

    add error handling for sendgrid api

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ app.use(cors(corsOptions))
 app.use(express.json());
 
 app.post('/', validateMail, (req, res) => {
-  try {
-    const { email, message } = req.body;
-    const body = {
-      to: process.env.recepient_email,
-      from: email,
-      subject: 'Portfolio Response from a Potential Client',
-      text: message,
-      html: `<strong>${message}</strong>`,
-    };
-    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
-    sgMail.send(body);
-
-    return res.status(200).send({ message: 'Email sent successfully' });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send({ message: 'Something went wrong. Try Again.' });
-  }
+  const { email, message } = req.body;
+  const body = {
+    to: process.env.recepient_email,
+    from: email,
+    subject: 'Portfolio Response from a Potential Client',
+    text: message,
+    html: `<strong>${message}</strong>`,
+  };
+  sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+  sgMail
+    .send(body)
+    .then((data) => {
+      res.status(200).send({ message: 'Email sent successfully' })})
+    .catch((error)=> {
+      console.log(error);
+      res.status(500).send({ message: 'Something went wrong. Try Again.' });
+    })
 });
 
 app.listen(3000, () => {
